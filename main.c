143af4ba1714e5c2723576df813e4212c0a8ee57
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-08-21 00:57:41 +0400

    Added cursor for some reason

--- main.c
+++ main.c
@@ -143,23 +143,59 @@ typedef struct
     Pager *pager;
 } Table;
 
+typedef struct
+{
+    Table *table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
+Cursor *table_start(Table *table)
+{
+    Cursor *cursor = (Cursor *)malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor *table_end(Table *table)
+{
+    Cursor *cursor = (Cursor *)malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void cursor_advance(Cursor *cursor)
+{
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows)
+    {
+        cursor->end_of_table = true;
+    }
+}
+
 Pager *pager_open(const char *filename)
 {
-#ifdef _WIN32
-    HANDLE hFile = CreateFile(filename,
-                              GENERIC_READ | GENERIC_WRITE,
-                              0,
-                              NULL,
-                              CREATE_NEW,
-                              FILE_ATTRIBUTE_NORMAL,
-                              NULL);
+    // #ifdef _WIN32
+    //     HANDLE hFile = CreateFile(filename,
+    //                               GENERIC_READ | GENERIC_WRITE,
+    //                               0,
+    //                               NULL,
+    //                               CREATE_NEW,
+    //                               FILE_ATTRIBUTE_NORMAL,
+    //                               NULL);
 
-    if (hFile == INVALID_HANDLE_VALUE)
-    {
-        printf("Unable to open file\n");
-        exit(EXIT_FAILURE);
-    }
-#endif
+    //     if (hFile == INVALID_HANDLE_VALUE)
+    //     {
+    //         printf("Unable to open file\n");
+    //         exit(EXIT_FAILURE);
+    //     }
+    // #endif
     int fd = open(filename,
                   O_RDWR |
                       O_CREAT,
@@ -429,11 +465,12 @@ void *get_page(Pager *pager, uint32_t page_num)
     return pager->pages[page_num];
 }
 
-void *row_slot(Table *table, uint32_t row_num)
+void *cursor_value(Cursor *cursor)
 {
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
 
-    void *page = get_page(table->pager, page_num);
+    void *page = get_page(cursor->table->pager, page_num);
 
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
@@ -448,21 +485,29 @@ ExecuteResult execute_insert(Statement *statement, Table *table)
     }
 
     Row *row_to_insert = &(statement->row_to_insert);
+    Cursor *cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
 
 ExecuteResult execute_select(Statement *statement, Table *table)
 {
+    Cursor *cursor = table_start(table);
+
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++)
+    while (!(cursor->end_of_table))
     {
-        deserialize_row(row_slot(table, i), &row);
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
